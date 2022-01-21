d97c8d7765bc1a173a5bc1fb11b83b9994bb35e7
dothash17 <82313804+dothash17@users.noreply.github.com>
2022-01-21 12:10:47 +0400

    fix

--- Model/Data/Service/UserService.cs
+++ Model/Data/Service/UserService.cs
@@ -1,18 +1,30 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace CourseWork.Model.Data.Service
 {
     public class UserService
     {
+        readonly static Dictionary<int, User> users = new();
         public static bool AuthUser(string nickname, string password)
         {
             using (ApplicationContext db = new())
             {
+                foreach (var item in db.Users)
+                    if (!users.ContainsKey(item.Id)) users.Add(item.Id, item);
+
                 bool isAuthenticated = db.Users.Any(u => u.Nickname == nickname & u.Password == password);
                 if (isAuthenticated) return true;
             }
             return false;
 
+        }
+        public static void CreateUser(string nickname, string password)
+        {
+            using ApplicationContext db = new();
+            bool isAuthenticated = db.Users.Any(u => u.Nickname == nickname & u.Password == password);
+            if (!isAuthenticated) db.Users.Add(new User() { Nickname = nickname, Password = password });
+            db.SaveChanges();
         }
     }
 }
